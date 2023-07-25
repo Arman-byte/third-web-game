2bdc0e4ce14d1a72afd9a7e0e2494d1c0ec910a2
Arman <80922038+Arman-byte@users.noreply.github.com>
2023-07-25 10:26:09 -0400

    Add files via upload

---
+++ application/const/contractAddresses.ts
@@ -0,0 +1,10 @@
+export const CHARACTER_EDITION_ADDRESS =
+  "Your Character address";
+
+export const MINING_CONTRACT_ADDRESS =
+  "Mining contract address (the Mining.sol file you deployed)";
+
+export const GOLD_GEMS_ADDRESS = "Your Salmon address";
+
+export const PICKAXE_EDITION_ADDRESS =
+  "Tools address";
